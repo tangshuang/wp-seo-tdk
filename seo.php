91c98ec8c25313b8220312318bf29312aa555b63
tangshuang <mail@tangshuang.net>
2019-12-25 23:11:38 +0800

    update

--- seo.php
+++ seo.php
@@ -4,7 +4,7 @@
 Plugin Name: WP SEO TDK
 Plugin URI: http://www.tangshuang.net/wp-seo
 Description: 为您提供一个通用的网页标题、关键词、描述方案，实现最基本的SEO TDK目的。
-Version: 2.0.2
+Version: 10.0.2
 Author: 否子戈
 Author URI: http://www.tangshuang.net
 */
